d1a0a23c099b29c8de0f368442e29f52c4ab01d3
barthaasbroek <bart@phoenixsite.nl>
2023-08-25 20:27:19 +0200

    fix

--- plugins/lightgallery.client.js
+++ plugins/lightgallery.client.js
@@ -1,17 +1,17 @@
-// import Lightgallery from 'lightgallery/vue/LightGalleryVue.umd.js';
-// import lgVideo from "lightgallery/plugins/video";
-// import s2 from 'lightgallery/css/lightgallery-bundle.min.css';
-// import s3 from 'lightgallery/css/lg-video.css';
+import Lightgallery from 'lightgallery/vue/LightGalleryVue.umd.js';
+import lgVideo from "lightgallery/plugins/video";
+import s2 from 'lightgallery/css/lightgallery-bundle.min.css';
+import s3 from 'lightgallery/css/lg-video.css';
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//     // nuxtApp.vueApp.use(Lightgallery)
-//     return {
-//         provide: {
-//           Lightgallery,
-//           lgVideo
-//         }
-//       }
-//   })
+export default defineNuxtPlugin((nuxtApp) => {
+    // nuxtApp.vueApp.use(Lightgallery)
+    return {
+        provide: {
+          Lightgallery,
+          lgVideo
+        }
+      }
+  })
 
 
 
