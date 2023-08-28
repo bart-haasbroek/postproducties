ec42a91ec29c53191f11a19197ca81e9f72adbd3
barthaasbroek <bart@phoenixsite.nl>
2023-08-28 21:17:44 +0200

    add animations

--- pages/animaties.vue
+++ pages/animaties.vue
@@ -9,25 +9,158 @@ const plugins = [lgVideo];
 <template>
   <div class="h-full">
     <Lightgallery
-      class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5"
+      class="grid grid-cols-1 md:grid-cols-2"
       :settings="{ speed: 500, plugins: plugins }"
     >
       <a
-        data-video='{"source": [{"src":"/animations/3d Pen.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
-        data-poster="/images/over-mij.jpg"
+        data-video='{"source": [{"src":"/animations/1 - Alutech - Aluminized - logo animatie.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb1-1536x960.png"
         class="relative cursor-pointer group"
       >
-        <img src="/images/over-mij.jpg" />
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb1-1536x960.png"
+        />
         <div
           class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
         ></div>
       </a>
+
       <a
-        data-video='{"source": [{"src":"/animations/Telefoon transfer testklein.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
-        data-poster="/images/over-mij.jpg"
+        data-video='{"source": [{"src":"/animations/2 - Telefoon transfer testklein.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb2-1536x864.png"
         class="relative cursor-pointer group"
       >
-        <img src="/images/05-16-2018-Marianne-12-2000x1200.jpg" />
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb2-1536x864.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/3 - 3d Pen.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb3-1536x866.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb3-1536x866.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/4 - Bird-3.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb4-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb4-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/5 - BonBon promo_HD.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb5-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb5-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/6 - Logo animatie dots.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb6-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb6-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/7 - De Watering_Logo lagen2.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb7-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb7-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/8 - LUMC logo.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb8-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb8-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/9 - Match cut - 01.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb9-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb9-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/10 - Match cut - 02.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb10-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb10-1536x960.png"
+        />
+        <div
+          class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
+        ></div>
+      </a>
+
+      <a
+        data-video='{"source": [{"src":"/animations/11 - google balletjes.mp4", "type":"video/mp4"}], "attributes": {"preload": true, "playsinline": true, "controls": false, "loop": true}}'
+        data-poster="/images/thumbs/animation-thumb11-1536x960.png"
+        class="relative cursor-pointer group"
+      >
+        <img
+          class="img-responsive w-full h-full object-cover object-top"
+          src="/images/thumbs/animation-thumb11-1536x960.png"
+        />
         <div
           class="absolute w-full h-full left-0 top-0 bg-[#0c6285] opacity-0 group-hover:opacity-50 transition-all duration-300"
         ></div>
--- pages/videos.vue
+++ pages/videos.vue
@@ -1,46 +1,7 @@
 <script lang="ts" setup>
 import Lightgallery from "lightgallery/vue/LightGalleryVue.umd.js";
-
 import lgVideo from "lightgallery/plugins/video/lg-video.umd.js";
-
 const plugins = [lgVideo];
-
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
-
-// const videos = [
-//     {
-//         poster: 'https://img.youtube.com/vi/EIUJfXk3_3w/maxresdefault.jpg',
-//         attr: {
-//             'data-src': '//www.youtube.com/watch?v=EIUJfXk3_3w',
-//             'data-poster': 'https://img.youtube.com/vi/EIUJfXk3_3w/maxresdefault.jpg'
-//         },
-//     }
-//     {
-//         poster: 'https://img.youtube.com/vi/EIUJfXk3_3w/maxresdefault.jpg',
-//         attr: {
-//             'data-video': '{"source": [{"src":"/videos/raindrops.mp4", "type":"video/mp4"}], "tracks": [{"src": "{/videos/title.txt", "kind":"captions", "srclang": "en", "label": "English", "default": "true"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}',
-//             'data-poster': 'https://img.youtube.com/vi/EIUJfXk3_3w/maxresdefault.jpg'
-//         },
-//     }
-// ]
 </script>
 
 <template>
